cdffc0d7af25fec6c3c719dc20ddb0b342cffb53
JaDar <43009557+onyashed@users.noreply.github.com>
2023-07-05 12:57:48 +0300

    Add stocks.csv for ray.py add py io and operators .py

---
+++ py_io_operators.py
@@ -0,0 +1,36 @@
+# Python basics
+# This is a single line comment.
+"""
+Author: Onyango Edwin
+email:onyango@verygood.co.ke
+Very Good Tech."""
+"""
+Block comments goes in here.
+another line goes underneath here.
+"""
+# Simple output to screen
+print('Hello, world!')
+# Simple input into system from keyboard.
+name = input('What is your name?\n')
+# Response back from system with input details displayed.
+print('Hi, %s.' % name)
+# Simple void method with a single parameter.
+def greet(name):
+    print ('Hello', name)
+
+greet('Sitini')
+greet('Under Pressure.')
+greet('Nabukwesi')
+# Arithmetic operators
+# Addition
+def add( a,  b):
+    return a+b;
+# the parameters need not be datatyped.
+print(add(1.1,2.90))
+def div(a,b):
+    if b==0:
+        print (" infinity")
+    else:
+        return a/b;
+print(div(0,2))
+# Handle Mul, sub and modulus.
