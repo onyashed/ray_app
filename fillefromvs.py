847e32044ad5e4ee405e74ed55c42c46d5143da4
Onyash Ed <onyashed@gmail.com>
2025-04-29 10:42:20 +0300

    Simple test on vs code.

--- fillefromvs.py
+++ fillefromvs.py
@@ -1,1 +1,12 @@
 #Hope to train more and more.
+#0108615840 Mwebi
+# This program adds up integers that have been passed as arguments in the command line
+import sys
+try:
+    total = sum(int(arg) for arg in sys.argv[1:])
+    print ('sum =', total)
+except ValueError:
+    print ('Please supply integer arguments')
+print(" hi there...")
+id=input(' what is your id \n')
+print(" my id is "+id)
